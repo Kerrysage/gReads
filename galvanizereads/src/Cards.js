c61be05e3b8ad81cb286c3d3c05cef222b58afc0
Kerry Abernathy <Kerrysage89@gmail.com>
2018-12-04 17:10:13 -0700

    added book descriptions to cards

--- galvanizereads/src/Cards.js
+++ galvanizereads/src/Cards.js
@@ -9,7 +9,7 @@ const Cards = () => (
         <Card.Header>Python in A Nutshell</Card.Header>
         <Card.Meta>Alex Martelli</Card.Meta>
         <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
+        This book offers Python programmers one place to look when they need help remembering or deciphering the syntax of this open source language and its many powerful but scantily documented modules. This comprehensive reference guide makes it easy to look up the most frequently needed information--not just about the Python language itself, but also the most frequently used parts of the standard library and the most important third-party extensions.
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
@@ -29,7 +29,7 @@ const Cards = () => (
         <Card.Header>Think Python</Card.Header>
         <Card.Meta>Allen B. Downey</Card.Meta>
         <Card.Description>
-          Molly wants to add you to the group <strong>musicians</strong>
+        If you want to learn how to program, working with Python is an excellent way to start. This hands-on guide takes you through the language a step at a time, beginning with basic programming concepts before moving on to functions, recursion, data structures, and object-oriented design. This second edition and its supporting code have been updated for Python 3.
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
@@ -48,7 +48,7 @@ const Cards = () => (
         <Image floated='right' size='mini' src='https://covers.oreillystatic.com/images/0636920085270/lrg.jpg' />
         <Card.Header>Learning React Native</Card.Header>
         <Card.Meta>Bonnie Eisenman</Card.Meta>
-        <Card.Description>Jenny requested permission to view your contact details</Card.Description>
+        <Card.Description>Get a practical introduction to React Native, the JavaScript framework for writing and deploying fully featured mobile apps that look and feel native. With this hands-on guide, you’ll learn how to build applications that target iOS, Android, and other mobile platforms instead of browsers. You’ll also discover how to access platform features such as the camera, user location, and local storage.</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className='ui two buttons'>
@@ -66,7 +66,7 @@ const Cards = () => (
         <Image floated='right' size='mini' src='https://covers.oreillystatic.com/images/0636920033769/lrg.jpg' />
         <Card.Header>You Don't Know: ES6 & Beyond</Card.Header>
         <Card.Meta>Kyle Simpson</Card.Meta>
-        <Card.Description>Jenny requested permission to view your contact details</Card.Description>
+        <Card.Description>No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this compact guide focuses on new features available in ECMAScript 6 (ES6), the latest version of the standard upon which JavaScript is built.</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className='ui two buttons'>
@@ -84,7 +84,7 @@ const Cards = () => (
         <Image floated='right' size='mini' src='https://covers.oreillystatic.com/images/0636920026327/lrg.jpg' />
         <Card.Header>You Don't Know JS: Scope & Closures</Card.Header>
         <Card.Meta>Kyle Simpson</Card.Meta>
-        <Card.Description>Jenny requested permission to view your contact details</Card.Description>
+        <Card.Description>No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. This concise yet in-depth guide takes you inside scope and closures, two core concepts you need to know to become a more efficient and effective JavaScript programmer. You’ll learn how and why they work, and how an understanding of closures can be a powerful part of your development skillset.</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className='ui two buttons'>
@@ -102,7 +102,7 @@ const Cards = () => (
         <Image floated='right' size='mini' src='https://covers.oreillystatic.com/images/0636920033752/lrg.jpg' />
         <Card.Header>You Don't Know JS: Async & Performance</Card.Header>
         <Card.Meta>Kyle Simpson</Card.Meta>
-        <Card.Description>Jenny requested permission to view your contact details</Card.Description>
+        <Card.Description>No matter how much experience you have with JavaScript, odds are you don’t fully understand the language. As part of the \"You Don’t Know JS\" series, this concise yet in-depth guide focuses on new asynchronous features and performance techniques—including Promises, generators, and Web Workers—that let you create sophisticated single-page web applications and escape callback hell in the process.</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <div className='ui two buttons'>
